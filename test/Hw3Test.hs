3c299bb9033a8a3ae605d93a05123b836c939930
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2025-07-19 01:45:47 -0700

    hw3 init

---
+++ test/Hw3Test.hs
@@ -0,0 +1,8 @@
+module Hw3Test where
+
+import Hw3.Golf
+import Test.HUnit
+
+testAll :: Test
+testAll =
+  TestList []
--- test/TestMain.hs
+++ test/TestMain.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Hw1Test
 import Hw2Test
+import Hw3Test
 import Test.Framework (defaultMain)
 import Test.Framework.Providers.HUnit (hUnitTestToTests)
 import Test.HUnit
@@ -12,5 +13,6 @@ main =
     hUnitTestToTests $
       TestList
         [ TestLabel "Hw1Test" Hw1Test.testAll,
-          TestLabel "Hw2Test" Hw2Test.testAll
+          TestLabel "Hw2Test" Hw2Test.testAll,
+          TestLabel "Hw3Test" Hw3Test.testAll
         ]
