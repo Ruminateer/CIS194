642ab60f947e12b4855a51b67e91b61fb04b7208
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2025-07-20 01:22:27 -0700

    skips and localMaxima

--- Hw3/Golf.hs
+++ Hw3/Golf.hs
@@ -1,1 +1,23 @@
 module Hw3.Golf where
+
+skip :: Int -> [a] -> [a]
+skip _ [] = []
+skip n (x : xs)
+  | n <= 0 = []
+  | otherwise = x : skip n (drop (n - 1) xs)
+
+skips :: [a] -> [[a]]
+skips l = [skip i $ drop (i - 1) l | i <- [1 .. (length l)]]
+
+trioWise :: [c] -> [(c, c, c)]
+trioWise (x : y : z : zs) = (x, y, z) : trioWise (y : z : zs)
+trioWise _ = []
+
+mid :: (a, b, c) -> b
+mid (_, y, _) = y
+
+midLargest :: (Ord a) => (a, a, a) -> Bool
+midLargest (x, y, z) = y > x && y > z
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima = map mid . filter midLargest . trioWise
--- test/Hw3Test.hs
+++ test/Hw3Test.hs
@@ -3,6 +3,67 @@ module Hw3Test where
 import Hw3.Golf
 import Test.HUnit
 
+testSkips :: Test
+testSkips =
+  TestList
+    [ TestCase
+        ( assertEqual
+            "skips ABCD"
+            ["ABCD", "BD", "C", "D"]
+            (skips "ABCD")
+        ),
+      TestCase
+        ( assertEqual
+            "skips hello!"
+            ["hello!", "el!", "l!", "l", "o", "!"]
+            (skips "hello!")
+        ),
+      TestCase
+        ( assertEqual
+            "skips [1]"
+            [[1]]
+            (skips [1 :: Int])
+        ),
+      TestCase
+        ( assertEqual
+            "skips [True, False]"
+            [[True, False], [False]]
+            (skips [True, False])
+        ),
+      TestCase
+        ( assertEqual
+            "skips []"
+            []
+            (skips [] :: [[Int]])
+        )
+    ]
+
+testLocalMaxima :: Test
+testLocalMaxima =
+  TestList
+    [ TestCase
+        ( assertEqual
+            "localMaxima [2, 9, 5, 6, 1]"
+            [9, 6]
+            (localMaxima [2, 9, 5, 6, 1])
+        ),
+      TestCase
+        ( assertEqual
+            "localMaxima [2, 3, 4, 1, 5]"
+            [4]
+            (localMaxima [2, 3, 4, 1, 5])
+        ),
+      TestCase
+        ( assertEqual
+            "localMaxima [1, 2, 3, 4, 5]"
+            []
+            (localMaxima [1, 2, 3, 4, 5])
+        )
+    ]
+
 testAll :: Test
 testAll =
-  TestList []
+  TestList
+    [ TestLabel "test skips" testSkips,
+      TestLabel "test localMaxima" testLocalMaxima
+    ]
