1272d2f8bcd828720ad1241e999ec4fc33782948
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2026-07-12 02:01:44 -0700

    hanoi rename and format

--- Hw1/Hanoi.hs
+++ Hw1/Hanoi.hs
@@ -5,19 +5,13 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi n a b c
+hanoi n src dst tmp
   | n <= 0 = []
-  | otherwise = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
+  | otherwise = hanoi (n - 1) src tmp dst ++ [(src, dst)] ++ hanoi (n - 1) tmp dst src
 
 hanoiAcc :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoiAcc n a b c = go n a b c []
+hanoiAcc n source dest temp = go n source dest temp []
   where
-    go k from to temp rest
+    go k src dst tmp rest
       | k <= 0 = rest
-      | otherwise =
-          go
-            (k - 1)
-            from
-            temp
-            to
-            ((from, to) : go (k - 1) temp to from rest)
+      | otherwise = go (k - 1) src tmp dst ((src, dst) : go (k - 1) tmp dst src rest)
