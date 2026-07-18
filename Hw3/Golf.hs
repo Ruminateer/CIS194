80e9de0ce691c98a666ab36c6364660b6601cbd1
zhangzhengyuan <zhangzhengyuan0604@gmail.com>
2026-07-18 01:26:35 -0700

    [hw3] complete behavior + unit tests

--- Hw3/Golf.hs
+++ Hw3/Golf.hs
@@ -1,5 +1,8 @@
 module Hw3.Golf where
 
+import qualified Data.Vector.Unboxed as VU
+
+-- Exercise 1 Hopscotch
 skip :: Int -> [a] -> [a]
 skip _ [] = []
 skip n (x : xs)
@@ -7,8 +10,9 @@ skip n (x : xs)
   | otherwise = x : skip n (drop (n - 1) xs)
 
 skips :: [a] -> [[a]]
-skips l = [skip i $ drop (i - 1) l | i <- [1 .. (length l)]]
+skips l = [skip i $ drop (i - 1) l | i <- [1 .. length l]]
 
+-- Exercise 2 Local maxima
 trioWise :: [c] -> [(c, c, c)]
 trioWise (x : y : z : zs) = (x, y, z) : trioWise (y : z : zs)
 trioWise _ = []
@@ -21,3 +25,19 @@ midLargest (x, y, z) = y > x && y > z
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima = map mid . filter midLargest . trioWise
+
+-- Exercise 3 Histogram
+
+histogram :: [Integer] -> String
+histogram nums = go (countNumbers nums) "==========\n0123456789\n"
+  where
+    countNumbers :: [Integer] -> [Int]
+    countNumbers = VU.toList . VU.accum (+) (VU.replicate 10 0) . map (\n -> (fromInteger n, 1))
+
+    go :: [Int] -> String -> String
+    go cnts acc
+      | all (== 0) cnts = acc
+      | otherwise = let (nextCnts, currentLine) = histogramLine cnts in go nextCnts (currentLine ++ '\n' : acc)
+
+    histogramLine :: [Int] -> ([Int], String)
+    histogramLine = unzip . map (\n -> if n > 0 then (n - 1, '*') else (n, ' '))
--- test/Hw3Test.hs
+++ test/Hw3Test.hs
@@ -1,59 +1,113 @@
 module Hw3Test where
 
-import Hw3.Golf
-import Test.Tasty
-import Test.Tasty.HUnit
+import Hw3.Golf (histogram, localMaxima, skips)
+import Test.Tasty (TestTree, testGroup)
+import Test.Tasty.HUnit (testCase, (@=?))
 
 testSkips :: TestTree
 testSkips =
   testGroup
     "skips"
-    [ testCase
-        "ABCD"
-        ( ["ABCD", "BD", "C", "D"]
-            @=? skips "ABCD"
-        ),
-      testCase
-        "hello!"
-        ( ["hello!", "el!", "l!", "l", "o", "!"]
-            @=? skips "hello!"
-        ),
-      testCase
-        "[1]"
-        ( [[1]]
-            @=? skips [1 :: Int]
-        ),
-      testCase
-        "[True, False]"
-        ( [[True, False], [False]]
-            @=? skips [True, False]
-        ),
-      testCase
-        "[]"
-        ( []
-            @=? (skips [] :: [[Int]])
-        )
+    [ testCase "empty input has empty output" $
+        [] @=? (skips [] :: [[Int]]),
+      testCase "singleton input" $
+        [[1]] @=? skips [1 :: Int],
+      testCase "two elements" $
+        [[True, False], [False]] @=? skips [True, False],
+      testCase "handout alphabet example" $
+        ["ABCD", "BD", "C", "D"] @=? skips "ABCD",
+      testCase "handout hello example" $
+        ["hello!", "el!", "l!", "l", "o", "!"] @=? skips "hello!",
+      testCase "odd-length input" $
+        [ [10, 20, 30, 40, 50],
+          [20, 40],
+          [30],
+          [40],
+          [50]
+        ]
+          @=? skips [10, 20, 30, 40, 50 :: Int],
+      testCase "repeated values retain their positions" $
+        [ [1, 1, 2, 1, 2, 2],
+          [1, 1, 2],
+          [2, 2],
+          [1],
+          [2],
+          [2]
+        ]
+          @=? skips [1, 1, 2, 1, 2, 2 :: Int]
     ]
 
 testLocalMaxima :: TestTree
 testLocalMaxima =
   testGroup
     "localMaxima"
-    [ testCase
-        "[2, 9, 5, 6, 1]"
-        ( [9, 6]
-            @=? localMaxima [2, 9, 5, 6, 1]
-        ),
-      testCase
-        "[2, 3, 4, 1, 5]"
-        ( [4]
-            @=? localMaxima [2, 3, 4, 1, 5]
-        ),
-      testCase
-        "[1, 2, 3, 4, 5]"
-        ( []
-            @=? localMaxima [1, 2, 3, 4, 5]
-        )
+    [ testCase "empty list" $
+        [] @=? localMaxima [],
+      testCase "one element cannot be a local maximum" $
+        [] @=? localMaxima [9],
+      testCase "two elements cannot contain a local maximum" $
+        [] @=? localMaxima [9, 1],
+      testCase "first and last elements are never local maxima" $
+        [2] @=? localMaxima [9, 1, 2, 1, 9],
+      testCase "single three-element peak" $
+        [2] @=? localMaxima [1, 2, 1],
+      testCase "handout multiple-peak example" $
+        [9, 6] @=? localMaxima [2, 9, 5, 6, 1],
+      testCase "handout middle-peak example" $
+        [4] @=? localMaxima [2, 3, 4, 1, 5],
+      testCase "strictly increasing list" $
+        [] @=? localMaxima [1, 2, 3, 4, 5],
+      testCase "strictly decreasing list" $
+        [] @=? localMaxima [5, 4, 3, 2, 1],
+      testCase "equal neighbors disqualify a candidate" $
+        [] @=? localMaxima [1, 2, 2, 1],
+      testCase "one equal neighbor is enough to disqualify" $
+        [] @=? localMaxima [1, 3, 3, 2, 2, 1],
+      testCase "negative values and peaks are returned in order" $
+        [-2, 0, -1] @=? localMaxima [-3, -2, -4, 0, -5, -1, -2]
+    ]
+
+testHistogram :: TestTree
+testHistogram =
+  testGroup
+    "histogram"
+    [ testCase "empty input contains only the axes" $
+        "==========\n0123456789\n" @=? histogram [],
+      testCase "one occurrence in the first column" $
+        "*         \n==========\n0123456789\n" @=? histogram [0],
+      testCase "one occurrence in the last column" $
+        "         *\n==========\n0123456789\n" @=? histogram [9],
+      testCase "one occurrence of every digit" $
+        "**********\n==========\n0123456789\n"
+          @=? histogram [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      testCase "handout first example" $
+        concat
+          [ " *        \n",
+            " *        \n",
+            " *   *    \n",
+            "==========\n",
+            "0123456789\n"
+          ]
+          @=? histogram [1, 1, 1, 5],
+      testCase "handout second example" $
+        concat
+          [ "    *     \n",
+            "    *     \n",
+            "    * *   \n",
+            " ******  *\n",
+            "==========\n",
+            "0123456789\n"
+          ]
+          @=? histogram [1, 4, 5, 4, 6, 6, 3, 4, 2, 4, 9],
+      testCase "rows are printed from highest level to lowest" $
+        concat
+          [ "*        *\n",
+            "* *      *\n",
+            "* *   *  *\n",
+            "==========\n",
+            "0123456789\n"
+          ]
+          @=? histogram [0, 0, 0, 2, 2, 6, 9, 9, 9]
     ]
 
 testAll :: TestTree
@@ -61,5 +115,6 @@ testAll =
   testGroup
     "Hw3"
     [ testSkips,
-      testLocalMaxima
+      testLocalMaxima,
+      testHistogram
     ]
